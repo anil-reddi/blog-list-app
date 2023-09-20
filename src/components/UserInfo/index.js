fc96939645b5eacd7b667896737e62db06a2304a
anil-reddi <j.anilreddy99@gmail.com>
2023-09-20 10:25:30 +0000

    first commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile-image"
+    />
+    <h1 className="profile-name">Wade Warren</h1>
+    <p className="profile-description">Software developer at Uk</p>
+  </div>
+)
+export default UserInfo
